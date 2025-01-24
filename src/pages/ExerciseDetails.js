99400a57e0bd107dc87cbd6a2b8ac25de63f33cf
Ayush Pal <ayush777pal2004@gmail.com>
2025-01-24 10:58:03 +0530

    exerciseDetail_and_befor_video

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import {Box} from '@mui/material'
+import {exerciseOptions, fetchData, youtubeOptions} from '../utils/fetchData'
+import Details from '../components/Details'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercise from '../components/SimilarExercise'
 
 const ExerciseDetails = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const { id }=useParams();
+  //useeffect to populate it
+  useEffect(()=>{
+    const fetchExercisesData = async ()=>{
+      const exerciseDbUrl='https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl= 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideoData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,youtubeOptions)
+      setExerciseVideos(exerciseVideoData);
+    }
+    fetchExercisesData();
+  },[id]);
+
   return (
-    <div>ExerciseDetails</div>
+    <Box>
+      <Details ExerciseDetail={exerciseDetail}/>
+        <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
+        <SimilarExercise/>
+    </Box>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,15 @@
 export const exerciseOptions = {
 	method: 'GET',
-	path: '/exercises?limit=67&offset=50',
 	headers: {
         'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
 		'x-rapidapi-key': 'b9827f76f2mshbe6fca9d173dcd6p121354jsn60e6568b2ed5',	
+	}
+};
+export const youtubeOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b9827f76f2mshbe6fca9d173dcd6p121354jsn60e6568b2ed5',
+		'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
 	}
 };
 // abstraction of data 
